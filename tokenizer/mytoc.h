6e2c4b22c0bd920ef1ebb3bafb5297a30f165810
kvillanueva <kvillanueva@miners.utep.edu>
2017-09-11 20:05:15 -0600

    Add files via upload

---
+++ tokenizer/mytoc.h
@@ -0,0 +1,10 @@
+/*Lab1: Tokenizer
+  Name: Kristian Villanueva
+  Last Modification: 9/11/17
+ */
+#ifndef MYTOC_H_
+#define MYTOC_H_
+
+char ** mytoc(char *str, char delim);
+
+#endif /* MYTOC_H_ */
